0c9c316491fff602822e17d23bb0f73b1d363a60
MrJonRoberts <jroberts@tas.qld.edu.au>
2022-02-10 09:19:30 +1000

    username pwd

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    name = "Mr Roberts"
-    return render_template("home.html", name=name)
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        # do something
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+        if username == "bob" and password == "1234":
+            return render_template("admin.html", name=username)
+        else:
+            return render_template("home.html", error="Wrong username and password")
+    else:
+
+        return render_template("home.html")
 
 
 if __name__ == '__main__':
